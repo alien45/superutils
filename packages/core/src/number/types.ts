9720e7df080a98258788dc457b1b4b5476ab4211
Toufiqur Chowdhury <htr.letun@gmail.com>
2025-12-26 15:14:02 +0300

    fix(core | promise | fetch): fixed `delayMs: 0` in deferred functions by improving type `PositiveNumber<T>` in core

--- packages/core/src/number/types.ts
+++ packages/core/src/number/types.ts
@@ -1,14 +1,20 @@
 /** Describes a number type with negative values only */
-export type NegativeNumber<N extends number = number> =
-	`${N}` extends `-${string}` ? N : never
+export type NegativeNumber<N> = N extends number
+	? `${N}` extends `-${string}`
+		? N
+		: never
+	: never
 
 /** Describes a number type with positive values excluding `0`  */
-export type PositiveNumber<N extends number = number> = `${N}` extends
-	| `-${string}`
-	| '0'
-	? never
-	: N
+export type PositiveNumber<N> = N extends number
+	? `${N}` extends `-${string}` | '0'
+		? never
+		: N
+	: never
 
 /** Describes a number type with positive values and `0`  */
-export type PositiveNumberWithZero<N extends number = number> =
-	`${N}` extends `-${string}` ? never : N
+export type PositiveNumberWithZero<N> = N extends number
+	? `${N}` extends `-${string}`
+		? never
+		: N
+	: never
