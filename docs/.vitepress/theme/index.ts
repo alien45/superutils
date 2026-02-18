9906c730e4d66cd6582cdc400123a38ef6f03c41
Toufiqur Chowdhury <htr.letun@gmail.com>
2026-02-18 10:36:04 +0300

    docs: fixed typo

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@ import { onContentUpdated } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { h, onMounted, onUnmounted, defineComponent } from 'vue'
 import Playground from './components/Playground.vue'
-import { addTryNowBtnNListen, replaceUrls, replacePageUrls } from './stackblitz'
+import { addTryNowBtnNListen, replacePageUrls } from './stackblitz'
 
 export default {
 	...DefaultTheme,
@@ -14,9 +14,8 @@ export default {
 					addTryNowBtnNListen()
 					replacePageUrls()
 				})
+				replacePageUrls()
 				addTryNowBtnNListen()
-
-				replaceUrls()
 			})
 			onUnmounted(() => {
 				// do stuff on application unmount
