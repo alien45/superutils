5501e3dd6323205c41ffc1d049c7080c7401d3f3
Toufiqur Chowdhury <htr.letun@gmail.com>
2026-05-11 06:49:08 +0300

    docs(core)

--- packages/core/src/iterable/filter.ts
+++ packages/core/src/iterable/filter.ts
@@ -9,9 +9,9 @@ import { IterableList } from './types'
  * @param predicate A function to test each element. Receives `(item, key, data)`.
  * @param limit (optional) maximum number of results to return. Defaults to `Infinity`.
  * @param asMap (optional) Whether to transform the result to an array or a map:
- * - `false`: array of values.
- * - `true` (default): Map with resptective keys/indexes preserved as map keys
- * @param result (optional) existing Map instance to populate.
+ * - `false` (default): array of values.
+ * - `true`: Map with resptective keys/indexes preserved as map keys
+ * @param result (optional) existing Map instance to populate the result.
  *
  * @template K The type of keys (Map) or indices (Array/Set) in the collection.
  * @template V The type of values in the collection.
@@ -33,11 +33,22 @@ import { IterableList } from './types'
  * ])
  *
  * // Result as a map  (default)
- * console.log(filter(map, item => item.age >= 30))
+ * const resultMap = filter(
+ *   map,
+ *   item => item.age >= 30, // predicate
+ *   2, // limit
+ *   true, // asMap
+ * )
+ * console.log(resultMap)
  * // Prints: Map(2) { 1 => { name: 'Alice', age: 30 }, 3 => { name: 'Charlie', age: 35 } }
  *
  * // Result as an array
- * console.log(filter(map, item => item.age >= 30), false)
+ * const resultArray = filter(
+ *   map,
+ *   item => item.age >= 30,
+ *   2,
+ * )
+ * console.log(resultArray)
  * // Prints: [{ name: 'Alice', age: 30 }, { name: 'Charlie', age: 35 }]
  * ```
  *
@@ -49,26 +60,36 @@ import { IterableList } from './types'
  * const numbers = [10, 20, 30, 40, 50]
  *
  * // Result as a map (default)
- * console.log(filter(numbers, n => n > 25, 2))
+ * const resultMap = filter(
+ *   numbers,
+ *   n => n > 25, // predicate
+ *   2, // limit
+ *   true, // asMap
+ * )
+ * console.log(resultMap)
  * // Prints: Map(2) { 2 => 30, 3 => 40 }
  *
  * // Result as an array
- * console.log(filter(numbers, n => n > 25, 2, false))
+ * const resultArray = filter(
+ *   numbers,
+ *   n => n > 25,
+ *   2,
+ * )
+ * console.log(resultArray)
  * // Prints: [30, 40]
  * ```
  */
-export const filter = <
-	K,
-	V,
-	AsMap extends boolean = false,
-	Result = AsMap extends true ? Map<K, V> : V[],
->(
-	data: IterableList<K, V>,
-	predicate: (item: V, key: K, data: IterableList<K, V>) => boolean,
+export const filter = <Key, Value, AsMap extends boolean = false>(
+	data: IterableList<Key, Value>,
+	predicate: (
+		item: Value,
+		key: Key,
+		data: IterableList<Key, Value>,
+	) => boolean,
 	limit?: number,
 	asMap?: AsMap,
-	result?: Map<K, V>,
-): Result => {
+	result?: Map<Key, Value>,
+) => {
 	if (!isMap(result)) result = new Map()
 
 	if (!isPositiveInteger(limit)) limit = Infinity
@@ -79,6 +100,8 @@ export const filter = <
 			&& result.set(key, item)
 	}
 
-	return (asMap ? result : [...result.values()]) as Result
+	return (asMap ? result : [...result.values()]) as AsMap extends true
+		? Map<Key, Value>
+		: Value[]
 }
 export default filter
